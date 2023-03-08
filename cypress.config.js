f73932e9676bfe56188859f4c0f5621ac02c1c49
Sudhagani <sdatchinamurthy@woolworths.com.au>
2023-03-08 12:27:43 +1100

    config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: '**/*.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
   },
 });
