fd201e83c6332b10a6bd378e725130682a5b7efb
Sudhagani <sdatchinamurthy@woolworths.com.au>
2023-03-08 17:26:52 +1100

    test

--- cypress.config.js
+++ cypress.config.js
@@ -5,10 +5,10 @@ module.exports = defineConfig({
     specPattern: '**/*.cy.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      require('cypress-email-results')(on, config, {
-        email: ['sudhadmurthy@gmail.com'],
-        transport,
-      })
+      // require('cypress-email-results')(on, config, {
+      //   email: ['sudhadmurthy@gmail.com'],
+      //   transport,
+      // })
       
     },
 
