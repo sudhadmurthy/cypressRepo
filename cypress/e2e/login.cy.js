a6af5b732f1428f010ecae90e686a99ec13ba56c
Sudhagani <sdatchinamurthy@woolworths.com.au>
2023-03-09 10:17:57 +1100

    performace test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,15 +4,31 @@ import loginPage from "../pageobjects/login"
 describe('login to herokuapp',()=>{
     let LoginPage = new loginPage();
     let data;
+    const expectedloadtimeinsec = 4
+
 before('run before',()=>
     {
-    cy.visit('https://the-internet.herokuapp.com/login');    
-cy.fixture('example').then( (fdata) => {
+        let start=0;
+        cy.then(()=>{
+            start= (performance.now())/1000;
+            cy.log("start"+start);
+        })
+    cy.visit('https://the-internet.herokuapp.com/login').then(()=>{
+        cy.log(`page load time: ${((performance.now())/1000) - start} seconds`);       
+        const pageloadtime = ((performance.now())/1000) - start
+        expect(pageloadtime).to.be.below(expectedloadtimeinsec)
+
+    });    
+
+
+    cy.fixture('example').then( (fdata) => {
     data = fdata})
     }
 )
 
     it('Login test',()=>{
+
+      
         //visit the url
         LoginPage.getEmail().type(data.userId);
         LoginPage.getPassword().type(data.password);
