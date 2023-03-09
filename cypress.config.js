f91e2717ac53ed74350b58414e3d014d7b41fcec
Sudhagani <sdatchinamurthy@woolworths.com.au>
2023-03-09 16:15:24 +1100

    test

--- cypress.config.js
+++ cypress.config.js
@@ -3,9 +3,32 @@ const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
   e2e: {
+
+
+    reporter: 'cypress-mochawesome-reporter',
+    reporterOptions: {
+      reporterEnabled: "mochawesome",
+      mochawesomeReporterOptions: {
+        overwrite: false,
+        reportDir: "results/reports/mocha",
+        reportFilename: "report",
+        reportTitle: "Test Automation Report",
+        showPassed: true,
+        showFailed: true,
+        showSkipped: false,
+        reportPageTitle: "Automation Report",
+        inline: true,
+        code: false,
+        charts: true,
+        html: true      
+      },
+      video: false},
+  
+
     specPattern: '**/*.cy.js',
     setupNodeEvents(on, config) {
 
+      require('cypress-mochawesome-reporter/plugin')(on);
 
       // implement node event listeners here
       // require('cypress-email-results')(on, config, {
@@ -19,7 +42,5 @@ module.exports = defineConfig({
   env: {
       allureReuseAfterSpec: true
   }
-
-
-  },
+  }
 });
